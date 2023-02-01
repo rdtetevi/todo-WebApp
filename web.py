30c27b901fa73934d230f47e7d07885039c95809
rodrigue.tetevi <rodrig.tetevi@gmail.com>
2023-02-01 02:40:51 +0100

    add, and delete element on todo app.

--- web.py
+++ web.py
@@ -41,7 +41,6 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-
 st.text_input(label="", placeholder="Add new todos...",
               on_change=add_todo, key='new_todo')
 
